e58feb4fbc06eb4d8441344a9313ee1feec95091
megasyl <lasjun_s@etna-alternance.net>
2017-06-07 10:47:08 +0200

    (FRONT) validation, debut creation crime

--- api/src/server.js
+++ api/src/server.js
@@ -12,7 +12,7 @@ const bodyParser   = require('body-parser');
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    //res.header('Content-Type', 'application/json');
+    res.header('Content-Type', 'application/json');
     next();
 });
 
